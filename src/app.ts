1d91a2a1895bd8322d3df907c76d07b56e060fd0
sheikh-riyadh <sheikhriyad350883@gmail.com>
2026-07-08 09:42:16 +0600

    created schema and model

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,32 @@
 import express, { Application, Request, Response } from "express";
+import { model, Schema } from "mongoose";
 
 const app: Application = express();
 
+const noteSchema = new Schema({
+  title: String,
+  content: String,
+});
 
+const Note = model("Note", noteSchema);
 
+app.post("/create-note", async (req: Request, res: Response) => {
+  const myNote = new Note({
+    title: "Learning mongoose",
+    content: "I am learning mongoose",
+  });
 
-app.get("/", async (req:Request, res:Response) => {
-    res.send("Welcome to mongoose practice")
-    
-})
+  await myNote.save()
 
+  res.status(201).json({
+    success: true,
+    message: "Note created successfuly",
+    note: myNote,
+  });
+});
 
-
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Welcome to mongoose practice");
+});
 
 export default app;
- 
--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const PORT = 5000;
 async function main() {
   try {
     await mongoose.connect(
-      "mongodb+srv://mongoose:TVZSETzaHTDyQsRe@cluster0.wjboujk.mongodb.net/mongoose_practice=Cluster0",
+      "mongodb+srv://mongoose:TVZSETzaHTDyQsRe@cluster0.wjboujk.mongodb.net/mongoose_practice",
     );
     console.log("Connected to mongodb")
     const server: Server = app.listen(PORT, () => {
